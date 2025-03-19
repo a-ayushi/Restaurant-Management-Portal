34b2bd36b6e5efea8a734617c246b8936a5428ea
a-ayushi <aayushipatidar07@gmail.com>
2025-03-19 14:10:19 +0530

    security config file modified so that only owners can access restaurants

--- src/main/java/com/restaurant/Restaurant/Management/Portal/security/SecurityConfig.java
+++ src/main/java/com/restaurant/Restaurant/Management/Portal/security/SecurityConfig.java
@@ -2,13 +2,13 @@ package com.restaurant.Restaurant.Management.Portal.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 public class SecurityConfig {
@@ -21,14 +21,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Use lambda to disable CSRF
+                .csrf(csrf -> csrf.disable()) // disabled for simplicity
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll() // Allow register & login
-                        .requestMatchers("/restaurants/**").hasAuthority("OWNER") // Owners only
+                        .requestMatchers("/restaurants/**").hasAuthority("OWNER") // Owners only can manage restaurants
                         .requestMatchers("/orders/**").hasAuthority("CUSTOMER") // Customers only
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.disable()) // Enable session-based authentication
+                .formLogin(withDefaults()) // Enable session-based authentication
+                // .logout(logout -> logout.logoutUrl("/auth/logout").logoutSuccessUrl("/auth/login"));
                 .logout(logout -> logout.logoutUrl("/auth/logout").logoutSuccessHandler((request,response,authentication)->{
                     response.setContentType("application/json");
                     response.getWriter().write("{\"message\": \"Logout successful!\"}");
