e0b74af288a8e8bac51c39fa880689bedea2e060
a-ayushi <aayushipatidar07@gmail.com>
2025-03-19 13:51:55 +0530

    renamed the findByOwner method to findByOwnerId

--- src/main/java/com/restaurant/Restaurant/Management/Portal/repository/RestaurantRepository.java
+++ src/main/java/com/restaurant/Restaurant/Management/Portal/repository/RestaurantRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long>{ // enable basic CRUD operaions
-  List<Restaurant> findByOwner(Long OwnerId); // fetch the restaurant by a specific owner
+  List<Restaurant> findByOwnerId(Long OwnerId); // fetch the restaurant by a specific owner
 }
