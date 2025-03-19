5d0fb0ec67d52babb30e5c9ea2c288dd61e43a29
a-ayushi <aayushipatidar07@gmail.com>
2025-03-19 13:35:50 +0530

    Restaurant repository for database operations

--- src/main/java/com/restaurant/Restaurant/Management/Portal/repository/RestaurantRepository.java
+++ src/main/java/com/restaurant/Restaurant/Management/Portal/repository/RestaurantRepository.java
@@ -1,4 +1,9 @@
 package com.restaurant.Restaurant.Management.Portal.repository;
 
-public interface RestaurantRepository {
+import com.restaurant.Restaurant.Management.Portal.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant,Long>{ // enable basic CRUD operaions
+  List<Restaurant> findByOwner(Long OwnerId); // fetch the restaurant by a specific owner
 }
